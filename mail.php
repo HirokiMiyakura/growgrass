0cdced3c069dfa94bcc54bc8d479ebf26156ff9a
HirokiMiyakura <miyakurahiroki@gmail.com>
2022-03-24 15:33:56 +0900

    20220323

--- mail.php
+++ mail.php
@@ -30,14 +30,14 @@ if (version_compare(PHP_VERSION, '5.1.0', '>=')) {//PHP5.1.0以上の場合の�
 //---------------------------　必須設定　必ず設定してください　-----------------------
 
 //サイトのトップページのURL　※デフォルトでは送信完了後に「トップページへ戻る」ボタンが表示されますので
-$site_top = "https://shibancyu-saitama.jp/";
+$site_top = "https://grow-grass.com/";
 
 //管理者のメールアドレス ※メールを受け取るメールアドレス(複数指定する場合は「,」で区切ってください 例 $to = "aa@aa.aa,bb@bb.bb";)
-$to = "info@shibancyu-saitama.jp";
+$to = "grow0513@outlook.jp";
 
 //自動返信メールの送信元メールアドレス
 //必ず実在するメールアドレスでかつ出来る限り設置先サイトのドメインと同じドメインのメールアドレスとすることを強く推奨します
-$from = "info@shibancyu-saitama.jp";
+$from = "grow0513@outlook.jp";
 
 //フォームのメールアドレス入力箇所のname属性の値（name="○○"　の○○部分）
 $Email = "Email";
